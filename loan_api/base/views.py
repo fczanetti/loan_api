4341ea3c166d4404c1c4a480a12c91309aa483ac
Fabio Zanetti <fczanetti@hotmail.com>
2024-07-02 14:27:00 -0300

    Restrict listing loan for auth users
    - Set token authentication;
    - create filter to only list loans from the requesting user;
    - adjusted tests.
    
    Close #14

--- loan_api/base/views.py
+++ loan_api/base/views.py
@@ -1,8 +1,16 @@
 from rest_framework import viewsets
 from loan_api.base.models import Loan
 from loan_api.base.serializers import LoanSerializer
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class LoanViewSet(viewsets.ModelViewSet):
-    queryset = Loan.objects.all().order_by('request_date')
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
     serializer_class = LoanSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        client = f'{user.username} - {user.pk}'
+        return Loan.objects.filter(client=client).order_by('request_date')
