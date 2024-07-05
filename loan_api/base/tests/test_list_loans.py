bd98b7b7462af2318211d67d72ca622d3ae53645
Fabio Zanetti <fczanetti@hotmail.com>
2024-07-05 16:25:27 -0300

    Implement loan update
    - Create tests for loan update;
    - create loan_01 and loan_02 and refactor some tests.
    
    Close #37

--- loan_api/base/tests/test_list_loans.py
+++ loan_api/base/tests/test_list_loans.py
@@ -1,9 +1,7 @@
 import pytest
 from rest_framework.test import APIClient
 from rest_framework.status import HTTP_200_OK, HTTP_401_UNAUTHORIZED
-from loan_api.base.models import Loan
 from loan_api.base.serializers import LoanSerializer
-from django.contrib.auth.models import User
 
 
 @pytest.fixture
@@ -24,14 +22,10 @@ def test_resp_loans_status_code(resp_list_loans_authenticated_user_test_1):
     assert resp_list_loans_authenticated_user_test_1.status_code == HTTP_200_OK
 
 
-def test_correct_loans_present_in_response(resp_list_loans_authenticated_user_test_1):
+def test_correct_loans_present_in_response(resp_list_loans_authenticated_user_test_1, loan_01, loan_02):
     """
     Certifies that only the loans from the user that made the request are present.
     """
-    user_01 = User.objects.get(username='User Test 1')
-    user_02 = User.objects.get(username='User Test 2')
-    loan_01 = Loan.objects.filter(client=user_01.username).first()
-    loan_02 = Loan.objects.filter(client=user_02.username).first()
     serializer_01 = LoanSerializer(loan_01)
     serializer_02 = LoanSerializer(loan_02)
     assert serializer_01.data in resp_list_loans_authenticated_user_test_1.json()['results']
---
+++ loan_api/base/tests/test_update_loan.py
@@ -0,0 +1,73 @@
+import pytest
+
+from loan_api.base.models import Loan
+from rest_framework.status import HTTP_200_OK, HTTP_404_NOT_FOUND, HTTP_401_UNAUTHORIZED, HTTP_400_BAD_REQUEST
+from rest_framework.test import APIClient
+from loan_api.base.serializers import LoanSerializer
+
+
+@pytest.fixture
+def resp_update_loan_authenticated_user_test_1(auth_client_user_test_1, loan_01, bank):
+    """
+    Creates a request updating a loan and returns a response.
+    """
+    data = {'value': 110, 'interest_rate': 2, 'bank': bank.pk}
+    resp = auth_client_user_test_1.put(f'/api/loans/{loan_01.pk}/', data=data)
+    return resp
+
+
+def test_resp_status_code_update_loan(resp_update_loan_authenticated_user_test_1):
+    """
+    Certifies that the update returns a response
+    with a 200 status code.
+    """
+    assert resp_update_loan_authenticated_user_test_1.status_code == HTTP_200_OK
+
+
+def test_updated_loan_returned_in_response(resp_update_loan_authenticated_user_test_1):
+    """
+    Certifies that the loan updated is returned
+    in the response.
+    """
+    loan = Loan.objects.filter(client='User Test 1').filter(value=110).first()
+    serializer = LoanSerializer(loan)
+    assert resp_update_loan_authenticated_user_test_1.json() == serializer.data
+
+
+def test_user_1_can_not_update_loan_from_user_2(auth_client_user_test_1, loan_02, bank):
+    """
+    Certifies that a user can not update a loan that
+    belongs to another user.
+    """
+    data = {'value': 110, 'interest_rate': 2, 'bank': bank.pk}
+    resp = auth_client_user_test_1.put(f'/api/loans/{loan_02.pk}/', data=data)
+    assert resp.status_code == HTTP_404_NOT_FOUND
+
+
+def test_unauthenticated_user_can_not_update_loans(loan_01, bank):
+    """
+    Certifies that a non authenticated user can not
+    update loans.
+    """
+    data = {'value': 110, 'interest_rate': 2, 'bank': bank.pk}
+    client = APIClient()
+    resp = client.put(f'/api/loans/{loan_01.pk}/', data=data)
+    assert resp.status_code == HTTP_401_UNAUTHORIZED
+
+
+def test_invalid_inputs_not_allowed(auth_client_user_test_1, loan_01, bank):
+    """
+    Certifies that invalid requests return a
+    400 status code response.
+    """
+    data_01 = {'value': '', 'interest_rate': 2, 'bank': bank.pk}        # Invalid value
+    data_02 = {'value': 120, 'interest_rate': 'a', 'bank': bank.pk}     # Invalid interest_rate
+    data_03 = {'value': 120, 'interest_rate': 3, 'bank': 123}         # Invalid bank
+
+    resp_01 = auth_client_user_test_1.put(f'/api/loans/{loan_01.pk}/', data=data_01)
+    resp_02 = auth_client_user_test_1.put(f'/api/loans/{loan_01.pk}/', data=data_02)
+    resp_03 = auth_client_user_test_1.put(f'/api/loans/{loan_01.pk}/', data=data_03)
+
+    assert resp_01.status_code == HTTP_400_BAD_REQUEST
+    assert resp_02.status_code == HTTP_400_BAD_REQUEST
+    assert resp_03.status_code == HTTP_400_BAD_REQUEST
